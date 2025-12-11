5e535281c5258c01d4f3c0945ba2eebc9fa92374
rohinimalla <24331a05g7@mvgrce.edu.in>
2025-12-11 23:14:21 +0530

    Add files via upload

---
+++ lab.py
@@ -0,0 +1,7 @@
+
+
+
+string1 = input("Enter the first string: ")
+string2 = input("Enter the second string: ")
+concatenated_string = string1 + string2
+print("The concatenated string is:", concatenated_string)
